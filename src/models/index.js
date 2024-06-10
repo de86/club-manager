59a264892a3fcf98a79217fac1f9946b2d331792
de86 <daelliott86@gmail.com>
2024-06-10 09:32:15 +0100

    - Removed logs directory

--- src/models/index.js
+++
@@ -1,5 +0,0 @@
-// Inject DB into all models. This allows models to be imported in other files
-// without needing to pass DB every time.
-module.exports = (db) => ({
-    User: require('./User').inject(db),
-});
